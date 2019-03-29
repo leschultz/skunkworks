2ec3c1781be9ad91bd6dfb8f716f8af83b7b2e7b
Lane Schultz <laneenriqueschultz@gmail.com>
2019-03-29 12:00:52 -0500

    Improved script

--- binary_oxide_reference_data/dataextraction.py
+++ binary_oxide_reference_data/dataextraction.py
@@ -2,28 +2,48 @@ from pymatgen.analysis.reaction_calculator import ComputedReaction
 
 import pymatgen as mg
 import pandas as pd
+import numpy as np
 
 import re
+
+
+def natoms(x):
+    '''
+    Sum the nubmer of atoms in a composition.
+    '''
+
+    return sum([j for i, j in x.items()])
 
 mpr = mg.MPRester()  # My API key for pymatgen
 
 epath = '../data/vaspdata_collected.xlsx'
 
 df = pd.read_excel(epath)  # Import data containing composition energy
-columns = list(df.columns)[:-2]  # Relevant columns
 
-df = df[columns]  # Compositions and energies
-df[columns[0]] = df[columns[0]].replace('\n', '', regex=True)
+df = df[['composition', 'energy']]
+df['composition'] = df['composition'].replace(' \n', '', regex=True)
 
-print(df)
+# Find the number of atoms in each composition
+atoms = df['composition'].apply(mg.Composition)
+atoms = atoms.apply(dict)
+atoms = atoms.apply(natoms)
 
-for row in df['composition']:
+df['atoms'] = atoms
+df['e_per_atom'] = df['energy']/df['atoms']
+
+df['oxides'] = np.nan
+df['oxide_e_per_atom'] = np.nan
+df['e_per_atom-sum(oxide_e_per_atom)'] = np.nan
+
+count = 0
+for row, e in zip(df['composition'], df['e_per_atom']):
     elements = re.findall('[A-Z][a-z]?|[0-9]+', row)
     elements = [i for i in elements if not i.isdigit()]
 
     oxides = [str(i)+'-O' for i in elements if str(i) != 'O']
 
     oxidechoices = []
+    energies = []
     for oxide in oxides:
         d = mpr.query(oxide, ['pretty_formula', 'e_above_hull', 'energy_per_atom'])
 
@@ -31,15 +51,17 @@ for row in df['composition']:
         d = d.sort_values(by=['e_above_hull', 'energy_per_atom'])
         d = d.reset_index(drop=True)
 
-        choice = d['pretty_formula'][0]
+        oxidechoices.append(d['pretty_formula'][0])
+        energies.append(d['energy_per_atom'][0])
 
-        oxidechoices.append(choice)
+    energysum = sum(energies)
 
+    diff = e-energysum
 
-    # Oxide energies
-    print(row)
-    print(oxidechoices)
-    reactants = [mpr.query(i, ['energy']) for i in oxidechoices]
+    df['oxides'].iloc[count] = oxides
+    df['oxide_e_per_atom'].iloc[count] = energysum
+    df['e_per_atom-sum(oxide_e_per_atom)'].iloc[count] = diff
 
-    print(reactants)
-    # Calculate reaction energies
+    count += 1
+
+df.to_csv('data.txt', index=False)
